d92eea79d3c41427176a2eccaf2319a95f053905
Luiz Dendena <luiz@debian>
2019-08-28 17:31:22 -0300

    Arquivo de conexão (apenas para teste)

--- _include/conexao.php
+++ _include/conexao.php
@@ -1,5 +1,5 @@
 <?php
-$conexao = mysqli_connect("localhost", "admin", "usgh*162Tjsi", "");
+$conexao = mysqli_connect("localhost", "admin", "usgh*162Tjsi", "control");
 mysqli_query($conexao, "SET NAMES 'utf8'");
 mysqli_query($conexao, "SET character_set_connection=utf8");
 mysqli_query($conexao, "SET character_set_client=utf8");
